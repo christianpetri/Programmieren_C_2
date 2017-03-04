1fee4fb51ce23cc705838929fa6dba6002bacc38
christianpetri <christianpetri@hotmail.com>
2017-03-04 14:43:35 +0100

    Aufgabe 1

--- Programmieren_C_2/Programmieren_C_2/Source.cpp
+++ Programmieren_C_2/Programmieren_C_2/Source.cpp
@@ -37,7 +37,7 @@ void aufgabe() {
 	Wertes ( der Kehrwert von x ist 1.0 / x) und speichern Sie diese in einem
 	separaten Array. Geben Sie jeden Kehrwert und die Summe aller Kehrwerte
 	auf der Konsole aus.*/
-	 
+	/* 
 	const int sizeOfArray = 5;
 	double werte[sizeOfArray];
 	double resultat[sizeOfArray];
@@ -52,7 +52,33 @@ void aufgabe() {
 		sum += resultat[i];
 	}
 	printf("Summer der Kehrwerte = %lf", sum); 
-	getchar();
+	*/
+	/*
+		Definieren Sie ein Array 'data' mit 100 Elementen vom Typ double.Schreiben
+		Sie eine Schleife, die die folgende Sequenz von Werten im Arrays speichert :
+		1 / (2 * 3 * 4), 1 / (4 * 5 * 6), 1 / (6 * 7 * 8) ... bis zu 1 / (200 * 201 * 202)
+		Schreiben Sie eine weitere Schleife, die das folgende errechnet : data[0] -
+		data[1] + data[2] - data[3] + ... - data[99].Multiplizieren Sie das Ergebnis
+		mit 4.0, fügen 3.0 und Geben Sie das Ergebnis auf der Konsole aus.
+		Erkennen Sie die Wert, den Sie bekommen ?
+	*/
+	double data[100];
+	double resultData = 0;
+	for (int i = 2; i <= 100; i+=2) {
+		data[i] = 1 / (double)((i)*(i + 1)*( i+ 2)); 
+	}
+	for (int i = 0; i < 100; i+=2) {
+		resultData += data[i];
+	}
+
+	for (int i = 1; i < 100; i+=2) {
+		resultData -= data[i];	 
+	}
+	resultData *= 4;
+	resultData += 3;
+	printf("Resultat data[] = %lf");
+
+		getchar();
 }
 
 void main() {
