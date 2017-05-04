497e4f8abba81b56fd61fb7e2f8de1e5458471c4
christianpetri <christianpetri@hotmail.com>
2017-05-04 22:22:41 +0200

    .

--- 20170504_Programmieren_C_Aufgabe/20170504_Programmieren_C_Aufgabe/Source.cpp
+++ 20170504_Programmieren_C_Aufgabe/20170504_Programmieren_C_Aufgabe/Source.cpp
@@ -5,20 +5,23 @@
 
 //Schreibe ein Programm, das mehrere eingegebene Sätze
 //alphabetisch sortiert.
-/*
-void bubblesort(char *array, size_t length) {
+ 
+char* bubblesort(char *array, size_t length) {
 	for (int i = 0; i < length - 1; ++i) {
 		for (int j = 0; j < length - i - 1; ++j) {
 			if (array[j] > array[j + 1])
 			{
-				int tmp = array[j];
+				char tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+
+				printf("\n%c", array[j]);
 			}
 		}
 	}
+	return array;
 }
-*/
+ 
 int main() {
 	const size_t buffer_size= 100;
 	char buffer[buffer_size] = {};
@@ -33,13 +36,23 @@ int main() {
 		} 
 		strcpy_s(satz[i], length , buffer);
 		//printf("%s", satz[i]); 
-		printf("%d", i);
+		//printf("\n%d\n", i);
 	}
-	puts("1");
-	//bubblesort(*satz, size_t(2));
+	 
+	//bubblesort(*satz, 2);
 	//calloc 
-	puts("2");
+	for (int i = 0; i < 2-1; i++) { 
+		if (*satz[i] > *satz[i + 1]) {
+			char *temp = satz[i];
+			satz[i] = satz[i + 1];
+			satz[i + 1] = temp;
+		}
+		
+	}
+	 
 	for (int i = 0; i < 2; i++) {
+		printf("%d\n", strlen(satz[i])-1);
+		printf("%s\n\n", satz[i]);
 		free(satz[i]);
 		satz[i] = NULL;
 	}
