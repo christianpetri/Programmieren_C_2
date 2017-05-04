ed1e1b41ee09389d3c0dec4ca56535a4490887fd
christianpetri <christianpetri@hotmail.com>
2017-05-04 21:28:31 +0200

    .

--- 20170504_Programmieren_C_Aufgabe/20170504_Programmieren_C_Aufgabe/Source.cpp
+++ 20170504_Programmieren_C_Aufgabe/20170504_Programmieren_C_Aufgabe/Source.cpp
@@ -5,26 +5,46 @@
 
 //Schreibe ein Programm, das mehrere eingegebene Sätze
 //alphabetisch sortiert.
-
+/*
+void bubblesort(char *array, size_t length) {
+	for (int i = 0; i < length - 1; ++i) {
+		for (int j = 0; j < length - i - 1; ++j) {
+			if (array[j] > array[j + 1])
+			{
+				int tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+	}
+}
+*/
 int main() {
 	const size_t buffer_size= 100;
 	char buffer[buffer_size] = {};
 	char* satz[100];
-	size_t length;
+	size_t length = 0;
 	for (int i = 0; i < 2; i++) {  
 		fgets(buffer, sizeof(buffer), stdin);
 		length = strnlen_s(buffer,buffer_size)+1;
 		satz[i] = (char*)malloc(length);
 		if (!satz[i]) {
-			
-		}
-		strcpy_s(satz[0], length , buffer);
-		printf("%s", satz[i]); 
+			break;
+		} 
+		strcpy_s(satz[i], length , buffer);
+		//printf("%s", satz[i]); 
+		printf("%d", i);
 	}
-	//calloc
-	 
-	free(satz);
+	puts("1");
+	//bubblesort(*satz, size_t(2));
+	//calloc 
+	puts("2");
+	for (int i = 0; i < 2; i++) {
+		free(satz[i]);
+		satz[i] = NULL;
+	}
 	 
 	getchar();
 	return 0;
 }
+
